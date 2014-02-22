9d9c0ce273950b9096f50c471e47d174be32f53a
Lee Munroe <lee@leemunroe.com>
2014-02-21 17:35:53 -0800

    Add mixpanel

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-	
+
+  $(".js-trackemail").click(function() {
+    var email = $("#mce-EMAIL").val();
+    var team = $("#mce-TEAM").val();
+    mixpanel.track("Create Team", {"email": email,"team": team});
+    console.log(email,team);
+  });
+
 });
